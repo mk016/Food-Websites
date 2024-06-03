6b3aa9b298e00ef0edd99b981eaa8eeab849e267
Mk016 <mahendrakumawat80224@gmail.com>
2024-06-04 03:28:11 +0530

    Making the FoodDash

---
+++ FoodDas/src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from './assets/components/Navbar'
+import './App.css'
+import Hero from './assets/components/Hero'
+
+function App() {
+  
+
+  return (
+    <>
+     <Navbar />
+     <Hero />
+    </>
+  )
+}
+
+export default App
+ 
---
+++ FoodDas/src/assets/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className="bg-orange-50 p-10 text-center ">
+      <h1 className='font-bold text-4xl'>
+        Quick and Tasty Food Delivered with a Dash of Speed
+      </h1>
+    </div>
+  )
+}
+
+export default Hero;
