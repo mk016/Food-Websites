05c9c86837ba4de047204f647346b3633536a829
Mk016 <mahendrakumawat80224@gmail.com>
2024-06-04 03:46:23 +0530

    app.jsx update

--- FoodDas/src/App.jsx
+++ FoodDas/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './assets/components/Navbar'
 import './App.css'
 import Hero from './assets/components/Hero'
+import Features from './assets/components/Features'
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
     <>
      <Navbar />
      <Hero />
+     <Features />
     </>
   )
 }
