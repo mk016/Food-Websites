bebd07dd69f0eafd529618130dfae2903baf33e8
Mk016 <mahendrakumawat80224@gmail.com>
2024-06-04 03:46:43 +0530

    add hero.jsx

--- FoodDas/src/assets/components/Hero.jsx
+++ FoodDas/src/assets/components/Hero.jsx
@@ -6,6 +6,11 @@ function Hero() {
       <h1 className='font-bold text-4xl'>
         Quick and Tasty Food Delivered with a Dash of Speed
       </h1>
+      <div>
+        <button className='rounded-full bg-orange-500 px-4 py-2 text-white mr-2'>Order Now</button>
+        <button className='border border-orange-500 rounded-full px-4 py-2 text-orange-500'>Track Order</button>
+        <img src="food-image.png" alt="Food" className="mx-auto mt-6 w-64" />
+      </div>
     </div>
   )
 }
