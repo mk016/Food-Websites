92b8a4526624a9dd86b740c520b65711f99ace58
Mk016 <mahendrakumawat80224@gmail.com>
2024-07-15 05:06:56 +0530

    a

--- FoodDas/src/components/Signup.jsx
+++ FoodDas/src/components/Signup.jsx
@@ -33,14 +33,14 @@ function Signup() {
               <button type="submit" className="w-full flex justify-center py-3 px-4 border border-transparent rounded-3xl shadow-sm text-md font-medium text-white font-bold hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 mt-8 bg-orange-600">Signup to Continue</button>
             </div>
             <div className="mt-4 text-center">
-              <p className="text-sm text-gray-600">Already have an account? <a href="#" className="font-medium text-orange-600 hover:text-orange-500">Log in</a></p>
+              <p className="text-sm text-gray-600">Already have an account? <a href="/login" className="font-medium text-orange-600 hover:text-orange-500">Log in</a></p>
             </div>
             <div className="flex justify-center space-x-4 mt-8">
-              <button type="button" className="flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
+              <button type="button" className="flex items-center px-6 py-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
                 <FontAwesomeIcon icon={faGoogle} className="mr-2 text-red-500" />
                 Login with Google
               </button>
-              <button type="button" className="flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
+              <button type="button" className="flex items-center px-6 py-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
                 <FontAwesomeIcon icon={faFacebook} className="mr-2 text-blue-500" />
                 Login with Facebook
               </button>
