8510e6aeab48b2aff110395efc231bf899112b6d
Mk016 <mahendrakumawat80224@gmail.com>
2024-07-15 05:06:53 +0530

    a

--- FoodDas/src/components/Navbar.jsx
+++ FoodDas/src/components/Navbar.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-
+import logoImage from '../img/logo.png';
 function Navbar() {
   return (
     <>
-      <nav className="bg-orange-100 p-4 flex justify-between items-center">
+      <nav className="orange-100 p-4 flex justify-between items-center" style={{ backgroundColor: '#FFEFE6' }}>
         <div>
-          <h1 className=" font-bold text-2xl text-orange-600 ml-6">Baby Food</h1>
+        <img src={logoImage} alt="logo" className="mx-auto  md:w-40"></img>
         </div>
         <ul className="flex : space-x-5">
-          <li className="hover:font-bold">Home</li>
-          <li className="hover:font-bold">Menu</li>
-          <li className="hover:font-bold">Service</li>
-          <li className="hover:font-bold">About Us</li>
+          <li className=" font-bold hover:text-orange-300">Home</li>
+          <li className="font-bold hover:text-orange-300">Menu</li>
+          <li className="font-bold hover:text-orange-300">Service</li>
+          <li className="font-bold hover:text-orange-300">About Us</li>
         </ul>
         <div className="flex items-center space-x-2">
-            <input type="text" placeholder="" className="p-1 border rounded"/>
+            <input type="text" placeholder="" className="p-1 "/>
             <img src="profile-icon.png" alt="Profile" className="w-8 h-8 rounded-full" />
         </div>
       </nav>
