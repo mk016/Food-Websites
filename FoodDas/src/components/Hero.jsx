375e0db5816f14922f249f9083e250fb6cf724f0
Mk016 <mahendrakumawat80224@gmail.com>
2024-07-15 05:06:48 +0530

    a

--- FoodDas/src/components/Hero.jsx
+++ FoodDas/src/components/Hero.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
+import FoodImage from '../img/Food.png';
 
 function Hero() {
   return (
-    <div className="bg-orange-50 p-10 text-right">
-      <div className='flex justify-between items-center'>
-        <div className='text-left'>
-          <h1 className='font-bold text-4xl'>
-            Quick and Tasty Food Delivered with a Dash of Speed
-          </h1>
+    <div className="min- flex flex-col md:flex-row" style={{ backgroundColor: '#FFEFE6' }}>
+      <div className="w-full md:w-1/2 flex mt-9 ml-4 justify-center p-4">
+        {/* Left side content */}
+        <div>
+          <h1 className="text-4xl font-bold mb-2"><a className='text-orange-500'>Quick</a> and<a className='text-orange-500'> Testy</a></h1>
+          <h1 className="text-4xl font-bold mb-2"><a className='text-orange-500'>Food Delivered</a> with<a className='text-orange-500'> a</a></h1>
+          <h1 className="text-4xl font-bold mb-4"><a className='text-orange-500'>Dash of</a> Speed</h1>
+         
         </div>
-        <div className='text-right'>
-          <button className='rounded-full bg-orange-500 px-4 py-2 text-white mr-2'>Order Now</button>
-          <button className='border border-orange-500 rounded-full px-4 py-2 text-orange-500'>Track Order</button>
+        <div>
+          <button className='block w-full py-4 px-10 border border-2xl rounded-3xl border-orange-500 text-white bg-orange-500'>
+            Order Now
+          </button>
+          <button className='block w-full mt-4 py-4 px-10 border-2  rounded-3xl border-orange-500 text-orange-500'>
+            Track Order
+          </button>
         </div>
       </div>
-      <div className='mt-6 flex justify-spacearound'>
-        <img src="../src/img/food.png" alt="Food" className="w-64" />
+      
+      <div className='w-full md:w-1/2 mt-4 ml-6'>
+        <div className=' items-center w-80 h-80 bg-cover bg-center' style={{ backgroundImage: `url(${FoodImage})` }}>
+          {/* Smaller image container */}
+        </div>
+
       </div>
     </div>
   );
