7c59afa7f939a3da636b47d0872848337ee201eb
Mk016 <mahendrakumawat80224@gmail.com>
2024-07-16 00:01:29 +0530

    a

--- text.jsx
+++ text.jsx
@@ -1,4 +1,40 @@
-<div className='text-right'>
-<button className='rounded-full bg-orange-500 px-4 py-2 text-white mr-2'>Order Now</button>
-<button className='border border-orange-500 rounded-full px-4 py-2 text-orange-500'>Track Order</button>
-</div>
+import React from "react";
+import fastDeliveryIcon from '../img/fast-delivery.png';
+import boxIcon from '../img/boxIcon.png';
+import freshIcon from '../img/freshIcon.png';
+
+function Features() {
+  return (
+    <div className="bg-pink-100 py-10 flex justify-center items-center">
+      <div className="bg-white rounded-xl shadow-lg p-6 flex justify-around max-w-5xl w-full">
+        <div className="flex items-center space-x-4 mx-2">
+          <img src={fastDeliveryIcon} alt="Fast Delivery" className="w-16 h-16" />
+          <div className="text-left">
+            <h1 className="text-xl font-bold mb-1">Fast Delivery</h1>
+            <p className="text-gray-500">Promise To Deliver</p>
+            <p className="text-gray-500">Within 30 Mins</p>
+          </div>
+        </div>
+        <div className="flex items-center space-x-4 mx-2">
+          <img src={freshIcon} alt="Fresh Food" className="w-16 h-16" />
+          <div className="text-left">
+            <h1 className="text-xl font-bold mb-1">Fresh Food</h1>
+            <p className="text-gray-500">Your Food Will Be Delivered</p>
+            <p className="text-gray-500">100% Fresh To Your Home.</p>
+          </div>
+        </div>
+        <div className="flex items-center space-x-4 mx-2">
+          <img src={boxIcon} alt="Free Delivery" className="w-16 h-16" />
+          <div className="text-left">
+            <h1 className="text-xl font-bold mb-1">Free Delivery</h1>
+            <p className="text-gray-500">Your Food Delivery Is</p>
+            <p className="text-gray-500">Absolutely Free. No Cost Just</p>
+            <p className="text-gray-500">Order</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Features;
