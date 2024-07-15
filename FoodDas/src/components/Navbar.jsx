317442430913c8ca7703d00fe9e907b874716521
Mk016 <mahendrakumawat80224@gmail.com>
2024-07-16 00:01:44 +0530

    a

--- FoodDas/src/components/Navbar.jsx
+++ FoodDas/src/components/Navbar.jsx
@@ -1,25 +1,29 @@
 import React from "react";
-import logoImage from '../img/logo.png';
+import logoImage from '../img/logo1.png';
+
 function Navbar() {
   return (
-    <>
-      <nav className="orange-100 p-4 flex justify-between items-center" style={{ backgroundColor: '#FFEFE6' }}>
-        <div>
-        <img src={logoImage} alt="logo" className="mx-auto  md:w-40"></img>
-        </div>
-        <ul className="flex : space-x-5">
-          <li className=" font-bold hover:text-orange-300">Home</li>
-          <li className="font-bold hover:text-orange-300">Menu</li>
-          <li className="font-bold hover:text-orange-300">Service</li>
-          <li className="font-bold hover:text-orange-300">About Us</li>
-        </ul>
-        <div className="flex items-center space-x-2">
-            <input type="text" placeholder="" className="p-1 "/>
-            <img src="profile-icon.png" alt="Profile" className="w-8 h-8 rounded-full" />
-        </div>
-      </nav>
-    </>
+    <nav className="bg-ornage-100 p-4 flex flex-wrap justify-between items-center" style={{ backgroundColor: '#FFEFE6' }}>
+      <div className="flex items-center">
+        <img src={logoImage} alt="logo" className="w-24 md:w-40" />
+      </div>
+      <ul className="flex flex-wrap gap-5 mt-4 md:mt-0">
+        <li className="font-bold hover:text-orange-500">Home</li>
+        <li className="font-bold hover:text-orange-500">Menu</li>
+        <li className="font-bold hover:text-orange-500">Service</li>
+        <li className="font-bold hover:text-orange-500">About Us</li>
+      </ul>
+      <div className="flex items-center mt-4 md:mt-0">
+        
+        <img
+          src="profile-icon.png"
+          alt="Profile"
+          className="w-8 h-8 rounded-full ml-2"
+        />
+      </div>
+    </nav>
   );
 }
 
 export default Navbar;
+
