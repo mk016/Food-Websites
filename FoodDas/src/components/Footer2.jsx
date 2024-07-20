0b617d0b8cb101741d11f9e9d30d57091da11e2e
Mk016 <mahendrakumawat80224@gmail.com>
2024-07-20 12:13:03 +0530

    a

--- FoodDas/src/components/Footer2.jsx
+++ FoodDas/src/components/Footer2.jsx
@@ -6,9 +6,12 @@ function Footer2() {
       <div className='flex mx-9 justify-between'>
         <div className='ml-9 '>
         <img src={logo1} alt='Footer-logo' className='mb-7'/>
-        <span className='text-gray-500 mt-4'>Food Dash ©2024 All Rights Reserved</span>
+        <span className='text-gray-500 mt-4 mb-5 '>Food Dash ©2024 All Rights Reserved</span>
         </div>
         <div>
+            <div>
+                <a href='#' title='facebook'><i className='fa fa-facebook-square' aria-hidden='true'></i></a>
+            </div>
             <span className='text-gray-500 '> By- Mahendra Kumawat </span>
         </div>
       </div>
