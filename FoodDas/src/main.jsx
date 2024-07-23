3c5ba75049a4620c27f4e9c11c2917e3e4066cc6
Mk016 <mahendrakumawat80224@gmail.com>
2024-07-24 01:48:12 +0530

    a

--- FoodDas/src/main.jsx
+++ FoodDas/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
 import App from './App.jsx';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
