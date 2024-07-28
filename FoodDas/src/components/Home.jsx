fc58ba99eb7f8e5da01e29dfac61fd15d750e18a
Mk016 <mahendrakumawat80224@gmail.com>
2024-07-29 04:30:40 +0530

    a

--- FoodDas/src/components/Home.jsx
+++ FoodDas/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './Navbar'
+
 import Hero from './Hero'
 import Features from './Features'
 import Footer from './Footer'
@@ -7,7 +7,7 @@ import Items from './items'
 function Home() {
   return (
     <div>
-      <Navbar />
+     
       <Hero />
       <Features />
       <Items />
