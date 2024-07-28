bc98599b52dc597b184f65fbdbab612961da8f95
Mk016 <mahendrakumawat80224@gmail.com>
2024-07-29 04:57:30 +0530

    a

--- FoodDas/src/components/Navbar.jsx
+++ FoodDas/src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import Cart from "./Cart";
 import logoImage from "../img/logo1.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import Login from "./login";
 import Menu from "./menu";
 
@@ -12,7 +14,13 @@ function Navbar() {
       style={{ backgroundColor: "#FFEFE6" }}
     >
       <div className="flex items-center">
-        <img src={logoImage} alt="logo" className="w-24 md:w-40" />
+      <Link to="/Home">
+              <img
+                src={logoImage}
+                alt="Food Dash Logo"
+                className="mx-auto w-24 md:w-40"
+              />{" "}
+            </Link>
       </div>
       <ul className="flex flex-wrap gap-5 mt-4 md:mt-0">
         <li className="text-gray-400 font-bold hover:text-orange-500">
@@ -67,8 +75,8 @@ function Navbar() {
         </li>
       </ul>
       <div className="flex items-center mt-4 md:mt-0">
-        <div className="bg-orange-500 p-4 rounded-full">
-          {/* <SearchIcon className="h-8 w-8 text-white" /> */}
+      <div className="bg-orange-500 h-8 w-8 rounded-full flex items-center justify-center">
+          <FontAwesomeIcon icon={faSearch} className="h-3 w-3 text-white" />
         </div>
         <Link
           to="/Cart"
