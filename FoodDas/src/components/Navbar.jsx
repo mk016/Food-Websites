5b0bbb738016c56dc50c9d9715393f1efc03f7e9
Mk016 <mahendrakumawat80224@gmail.com>
2024-07-29 04:30:51 +0530

    a

--- FoodDas/src/components/Navbar.jsx
+++ FoodDas/src/components/Navbar.jsx
@@ -1,39 +1,68 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import Cart from "./Cart";
 import logoImage from "../img/logo1.png";
-import Login from './login'
-import Menu from './menu'
+import Login from "./login";
+import Menu from "./menu";
 
 function Navbar() {
   return (
-    <nav className="bg-ornage-100 p-4 flex flex-wrap justify-between items-center" style={{ backgroundColor: '#FFEFE6' }}>
+    <nav
+      className="bg-ornage-100 p-4 flex flex-wrap justify-between items-center"
+      style={{ backgroundColor: "#FFEFE6" }}
+    >
       <div className="flex items-center">
         <img src={logoImage} alt="logo" className="w-24 md:w-40" />
       </div>
       <ul className="flex flex-wrap gap-5 mt-4 md:mt-0">
-        <li>
-          <NavLink to="/" className={(e)=>{e.isActive?"text-gray-300": "text-black"}}>
+        <li className="text-gray-400 font-bold hover:text-orange-500">
+          <NavLink
+            to="/"
+            className={(e) => {
+              e.isActive ? "text-gray-300" : "text-black";
+            }}
+          >
             Home
           </NavLink>
         </li>
-        <li>
-          <NavLink to="/Menu" className={(e)=>{e.isActive?"text-gray-300": "text-black"}}>
+        <li className="text-gray-400 font-bold hover:text-orange-500">
+          <NavLink
+            to="/Menu"
+            className={(e) => {
+              e.isActive ? "text-gray-300 font-bold" : "text-black";
+            }}
+          >
             Menu
           </NavLink>
         </li>
-        <li>
-          <NavLink to="/Offers" className={(e)=>{e.isActive?"text-gray-300": "text-black"}}>
+        <li className="text-gray-400 font-bold hover:text-orange-500">
+          <NavLink
+            to="/"
+            className={(e) => {
+              e.isActive ? "text-gray-300" : "text-black";
+            }}
+          >
             Offers
           </NavLink>
         </li>
-        <li>
-          <NavLink to="" className={(e)=>{e.isActive?"text-gray-300": "text-black"}}>
+        <li className="text-gray-400 font-bold hover:text-orange-500">
+          <NavLink
+            to="/"
+            className={(e) => {
+              e.isActive ? "text-gray-300" : "text-black";
+            }}
+          >
             Service
           </NavLink>
         </li>
-        <li>
-          <NavLink to="/Login" className={(e)=>{e.isActive?"text-gray-300": "text-black"}}>
-           Login
+        <li className="text-gray-400 font-bold hover:text-orange-500">
+          <NavLink
+            to="/Login"
+            className={(e) => {
+              e.isActive ? "text-gray-300" : "text-black";
+            }}
+          >
+            Login
           </NavLink>
         </li>
       </ul>
@@ -41,11 +70,10 @@ function Navbar() {
         <div className="bg-orange-500 p-4 rounded-full">
           {/* <SearchIcon className="h-8 w-8 text-white" /> */}
         </div>
-        <img
-          src="profile-icon.png"
-          alt="Profile"
-          className="w-8 h-8 rounded-full ml-2"
-        />
+        <Link
+          to="/Cart"
+          className="w-8 h-8 rounded-full bg-pink-200 ml-2"
+        ></Link>
       </div>
     </nav>
   );
