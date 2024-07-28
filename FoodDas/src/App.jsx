732108d868e7b844b85cc058f19ee515ac3e1660
Mk016 <mahendrakumawat80224@gmail.com>
2024-07-29 04:30:26 +0530

    a

--- FoodDas/src/App.jsx
+++ FoodDas/src/App.jsx
@@ -3,16 +3,21 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import Menu from "./components/menu";
+import Cart from "./components/Cart";
 import Home from "./components/Home";
 import Login from "./components/login";
+import Signup from "./components/Signup"; // Import the Signup component
 
 function App() {
   return (
     <>
+      <Navbar /> {/* Render Navbar globally */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<><Navbar /><Login /></>} />
+        <Route path="/login" element={<Login />} />
         <Route path="/menu" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/signup" element={<Signup />} /> {/* Add the Signup route */}
       </Routes>
     </>
   );
