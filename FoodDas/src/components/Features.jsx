8ab71fe4d03abf0409881d30bd691a43ba5c01ed
Mk016 <mahendrakumawat80224@gmail.com>
2024-08-03 01:12:07 +0530

    a

--- FoodDas/src/components/Features.jsx
+++ FoodDas/src/components/Features.jsx
@@ -5,31 +5,30 @@ import freshIcon from '../img/freshIcon.png';
 
 function Features() {
   return (
-    <div className='flex justify-center items-center py-full'>
-      <div className='flex justify-around bg-white rounded-xl shadow-lg mx-w-5xl w-full'>
-        <div className='flex items-center space-x-6 mx-6'>
-        <img src={fastDeliveryIcon} alt="Fast Delivery" className="w-16 h-16" />
+    <div className="flex justify-center items-center py-2 overflow-x-auto">
+      <div className="flex justify-around bg-white rounded-xl shadow-lg max-w-5xl w-full min-w-[300px] gap-4 sm:min-w-full">
+        <div className="flex items-center space-x-6 m-4">
+          <img src={fastDeliveryIcon} alt="Fast Delivery" className="w-16 h-16" />
           <div className="text-left">
-            <h1 className="text-xl font-bold mb-1 sm:text-sm ">Fast Delivery</h1>
-            <p className="text-gray-500">Promise To Deliver</p>
-            <p className="text-gray-500">Within 30 Mins</p>
+            <h1 className="text-xl font-bold mb-1">Fast Delivery</h1>
+            <p className="text-gray-500 hidden sm:block">Promise To Deliver</p>
+            <p className="text-gray-500 hidden sm:block">Within 30 Mins</p>
           </div>
         </div>
-        <div className="flex items-center space-x-4 mx-2">
+        <div className="flex items-center space-x-6 m-4">
           <img src={freshIcon} alt="Fresh Food" className="w-16 h-16" />
           <div className="text-left">
-            <h1 className="text-xl font-bold mb-1 sm:text-sm">Fresh Food</h1>
-            <p className="text-gray-500">Your Food Will Be Delivered</p>
-            <p className="text-gray-500">100% Fresh To Your Home.</p>
+            <h1 className="text-xl font-bold mb-1">Fresh Food</h1>
+            <p className="text-gray-500 hidden sm:block">Your Food Will Be Delivered</p>
+            <p className="text-gray-500 hidden sm:block">100% Fresh To Your Home.</p>
           </div>
         </div>
-        <div className="flex items-center space-x-4 mx-2">
+        <div className="flex items-center space-x-6 m-4">
           <img src={boxIcon} alt="Free Delivery" className="w-16 h-16" />
           <div className="text-left">
-            <h1 className="text-xl font-bold mb-1 sm:text-sm">Free Delivery</h1>
-            <p className="text-gray-500">Your Food Delivery Is</p>
-            <p className="text-gray-500">Absolutely Free. No Cost Just Order</p>
-            <p className="text-gray-500"></p>
+            <h1 className="text-xl font-bold mb-1">Free Delivery</h1>
+            <p className="text-gray-500 hidden sm:block">Your Food Delivery Is</p>
+            <p className="text-gray-500 hidden sm:block">Absolutely Free. No Cost Just Order</p>
           </div>
         </div>
       </div>
