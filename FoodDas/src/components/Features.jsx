f6c25897fb05de080472cc034e9f31a4c649aa9b
Mk016 <mahendrakumawat80224@gmail.com>
2024-08-02 23:33:34 +0530

    a

--- FoodDas/src/components/Features.jsx
+++ FoodDas/src/components/Features.jsx
@@ -10,7 +10,7 @@ function Features() {
         <div className='flex items-center space-x-6 mx-6'>
         <img src={fastDeliveryIcon} alt="Fast Delivery" className="w-16 h-16" />
           <div className="text-left">
-            <h1 className="text-xl font-bold mb-1">Fast Delivery</h1>
+            <h1 className="text-xl font-bold mb-1 sm:text-sm ">Fast Delivery</h1>
             <p className="text-gray-500">Promise To Deliver</p>
             <p className="text-gray-500">Within 30 Mins</p>
           </div>
@@ -18,7 +18,7 @@ function Features() {
         <div className="flex items-center space-x-4 mx-2">
           <img src={freshIcon} alt="Fresh Food" className="w-16 h-16" />
           <div className="text-left">
-            <h1 className="text-xl font-bold mb-1">Fresh Food</h1>
+            <h1 className="text-xl font-bold mb-1 sm:text-sm">Fresh Food</h1>
             <p className="text-gray-500">Your Food Will Be Delivered</p>
             <p className="text-gray-500">100% Fresh To Your Home.</p>
           </div>
@@ -26,7 +26,7 @@ function Features() {
         <div className="flex items-center space-x-4 mx-2">
           <img src={boxIcon} alt="Free Delivery" className="w-16 h-16" />
           <div className="text-left">
-            <h1 className="text-xl font-bold mb-1">Free Delivery</h1>
+            <h1 className="text-xl font-bold mb-1 sm:text-sm">Free Delivery</h1>
             <p className="text-gray-500">Your Food Delivery Is</p>
             <p className="text-gray-500">Absolutely Free. No Cost Just Order</p>
             <p className="text-gray-500"></p>
