fc988c4d1f85c4cc071dd217fbd3f17e6ef6ccbd
Mk016 <mahendrakumawat80224@gmail.com>
2024-08-02 23:33:31 +0530

    a

--- FoodDas/src/components/Navbar.jsx
+++ FoodDas/src/components/Navbar.jsx
@@ -1,34 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
-import Cart from "./Cart";
 import logoImage from "../img/logo1.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import Login from "./login";
-import Menu from "./menu";
+import { faSearch, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav
-      className="bg-ornage-100 p-4 flex flex-wrap justify-between items-center"
+      className="bg-orange-100 p-4 flex flex-wrap justify-between items-center"
       style={{ backgroundColor: "#FFEFE6" }}
     >
-      <div className="flex items-center">
-      <Link to="/Home">
-              <img
-                src={logoImage}
-                alt="Food Dash Logo"
-                className="mx-auto w-24 md:w-40"
-              />{" "}
-            </Link>
+      <div className="flex items-center justify-between w-full md:w-auto">
+        <Link to="/Home">
+          <img
+            src={logoImage}
+            alt="Food Dash Logo"
+            className="w-24 md:w-40"
+          />
+        </Link>
+        <button
+          className="md:hidden text-gray-400 focus:outline-none"
+          onClick={toggleMenu}
+        >
+          <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} className="h-6 w-6" />
+        </button>
       </div>
-      <ul className="flex flex-wrap gap-5 mt-4 md:mt-0">
+      <ul
+        className={`${
+          menuOpen ? "flex" : "hidden"
+        } flex-col md:flex md:flex-row gap-5 mt-4 md:mt-0 w-full md:w-auto`}
+      >
         <li className="text-gray-400 font-bold hover:text-orange-500">
           <NavLink
             to="/"
-            className={(e) => {
-              e.isActive ? "text-orange-500" : "text-black";
-            }}
+            className={(e) => (e.isActive ? "text-orange-500" : "text-black")}
           >
             Home
           </NavLink>
@@ -36,9 +47,7 @@ function Navbar() {
         <li className="text-gray-400 font-bold hover:text-orange-500">
           <NavLink
             to="/Menu"
-            className={(e) => {
-              e.isActive ? "text-gray-300 font-bold" : "text-black";
-            }}
+            className={(e) => (e.isActive ? "text-gray-300 font-bold" : "text-black")}
           >
             Menu
           </NavLink>
@@ -46,9 +55,7 @@ function Navbar() {
         <li className="text-gray-400 font-bold hover:text-orange-500">
           <NavLink
             to="/"
-            className={(e) => {
-              e.isActive ? "text-gray-300" : "text-black";
-            }}
+            className={(e) => (e.isActive ? "text-gray-300" : "text-black")}
           >
             Offers
           </NavLink>
@@ -56,9 +63,7 @@ function Navbar() {
         <li className="text-gray-400 font-bold hover:text-orange-500">
           <NavLink
             to="/"
-            className={(e) => {
-              e.isActive ? "text-gray-300" : "text-black";
-            }}
+            className={(e) => (e.isActive ? "text-gray-300" : "text-black")}
           >
             Service
           </NavLink>
@@ -66,22 +71,17 @@ function Navbar() {
         <li className="text-gray-400 font-bold hover:text-orange-500">
           <NavLink
             to="/Login"
-            className={(e) => {
-              e.isActive ? "text-gray-300" : "text-black";
-            }}
+            className={(e) => (e.isActive ? "text-gray-300" : "text-black")}
           >
             Login
           </NavLink>
         </li>
       </ul>
-      <div className="flex items-center mt-4 md:mt-0">
-      <div className="bg-orange-500 h-8 w-8 rounded-full flex items-center justify-center">
+      <div className="hidden lg:flex items-center mt-4 md:mt-0 w-full md:w-auto justify-between">
+        <div className="bg-orange-500 h-8 w-8 rounded-full flex items-center justify-center">
           <FontAwesomeIcon icon={faSearch} className="h-3 w-3 text-white" />
         </div>
-        <Link
-          to="/Cart"
-          className="w-8 h-8 rounded-full bg-pink-200 ml-2"
-        ></Link>
+        <Link to="/Cart" className="w-8 h-8 rounded-full bg-pink-200 ml-2"></Link>
       </div>
     </nav>
   );
